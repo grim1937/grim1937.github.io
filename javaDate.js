40599d0ab333c8d274a980840e077cc9700e2b73
grim1937 <31301803+grim1937@users.noreply.github.com>
2017-11-08 02:48:45 -0600

    jimmy

--- javaDate.js
+++ javaDate.js
@@ -1,2 +1,24 @@
 // JavaScript source code
-sysem.out.println("Hello World");
+
+// this function gets the date
+function renderTime() {
+    // Date
+    var mydate = new Date();
+    var year = mydate.getYear();
+        if (year < 1000) {
+            year += 1900;
+    }
+    var day = mydate.getDay();
+    var month = mydate.getMonth();
+    var daym = mydate.getDate();
+    var dayarray = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+    var montharray = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+    // Date End
+
+    var myClock = document.getElementById("clockDisplay");
+    myClock.textContent = "" + montharray[month] + " " + daym + " " + dayarray[day] + " " + year;
+    myClock.innerText = "" + montharray[month] + " " + daym + " " + dayarray[day] + " " + year;
+
+    setTimeout("renderTime()", 1000);
+}
+renderTime();
